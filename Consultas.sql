8a9bfe99d8f265f946902076968c6c765447fca7
Nicolas Vizcarra <nicolasvizcarra64@gmail.com>
2025-05-02 12:08:28 -0300

    Seleccionar números pares y excluir duplicados (DISTINCT)

--- Consultas.sql
+++ Consultas.sql
@@ -1,3 +1,7 @@
 --Seleccionar números pares
 SELECT BusinessEntityID, FirstName, LastName FROM Person.Person
 WHERE BusinessEntityID % 2 = 0;
+
+--Seleccionar números pares y excluir duplicados (DISTINCT)
+SELECT DISTINCT BusinessEntityID, FirstName, LastName FROM Person.Person
+WHERE BusinessEntityID % 2 = 0;
