c133599fce3611ee4d4477c4210c167acb41075b
Nicolas Vizcarra <nicolasvizcarra64@gmail.com>
2025-05-02 12:21:43 -0300

    Diferencia entre la cantidad total de nombres y la cantidad de DISTINCT nombres

--- Consultas.sql
+++ Consultas.sql
@@ -5,3 +5,12 @@ WHERE BusinessEntityID % 2 = 0;
 --Seleccionar números pares y excluir duplicados (DISTINCT)
 SELECT DISTINCT BusinessEntityID, FirstName, LastName FROM Person.Person
 WHERE BusinessEntityID % 2 = 0;
+
+--Diferencia entre la cantidad total de nombres y la cantidad de DISTINCT nombres
+SELECT
+	COUNT(FirstName) - COUNT(DISTINCT FirstName) AS NAME_ROWS_DIFFERENCE
+FROM Person.Person;
+
+SELECT 
+	COUNT(CITY) - COUNT(DISTINCT CITY) AS CITY_ROWS_DIFFERENCE
+FROM STATION;
